ba97c8ea9bc5ae16f121af6ebdea8e6b845ef407
ozway <lbrown60@vols.utk.edu>
2014-12-12 14:19:35 -0500

    condensed mlogit calculation for potential speedup

--- src/stable_exp.c
+++ src/stable_exp.c
@@ -32,39 +32,9 @@ void e_step_with_stable_exp(int *K, double *a_Z_normalized, double *total_sum, d
 		if the distance between the two phi values is 100 orders
 		of magnitude, the precision lost is irrelevant.			*/
 
-	for(k = 0; k < *K; k++){
-		a_Z_normalized[k] -= max_exp;
-	}
-
-
-
-	/* tmp_exp = HUGE_VAL for overflow and 0 for underflow.
-	 *   e.g. max_exp is large when parameters are near the boundary and is tiny when too many products.
-	 *        errno = ERANGE, only when tmp_exp is too huge (HUGE_VAL).
-	 *        errno = 0, when tmp_exp is too small. "Avoid to use ERANGE to check overflow or underflow!"
-	 * Scale max_exp by 2 such that close to +0 or -0 until errno is not ERANGE or tmp_exp is not HUGE_VAL. */
-/*	
-	tmp_exp = exp(max_exp);
-	if(tmp_exp == HUGE_VAL || tmp_exp == 0.0){
-		*flag_out_range = 1;
-		*scale_exp = (tmp_exp == HUGE_VAL) ? max_exp : -max_exp;
- 
-		do{
-			*scale_exp *= 0.5;
-			tmp_exp = exp(*scale_exp);
-		} while(tmp_exp == HUGE_VAL);
-		*scale_exp = max_exp - *scale_exp;
- 
-	}
-	if(*flag_out_range){
-		for(k = 0; k < *K; k++){
-			a_Z_normalized[k] -= *scale_exp;
-		}
-	}
-*/
 	*total_sum = 0.0;
 	for(k = 0; k < *K; k++){
-		a_Z_normalized[k] = exp(a_Z_normalized[k]);
+		a_Z_normalized[k] = exp(a_Z_normalized[k] - max_exp);
 		*total_sum += a_Z_normalized[k];
 	}
 	for(k = 0; k < *K; k++){
